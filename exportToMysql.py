bfd643726a7bf2ef2c56f764ce123311f11cd4b0
mickvanhulst <mick.vanhulst@gmail.com>
2016-10-05 12:26:40 +0200

    OMDB api scraper

--- exportToMysql.py
+++ exportToMysql.py
@@ -5,11 +5,11 @@ import pymysql
 #connect to database
 Connection = pymysql.connect(host='127.0.0.1', user="root", db='test', charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
-csvNames = {'links', 'movies', 'ratings', 'tags'}
+csvNames = {'movies', 'links', 'tags', 'ratings'}
 
 #Loop through data files and export to MySQL db
 for csv in csvNames:
-    file = pd.read_csv('../data/' + csv + '.csv')
+    file = pd.read_csv('./data/' + csv + '.csv')
     file.to_sql(con=Connection, name=''+csv+'', if_exists='replace', flavor='mysql')
 
 #Close connection
---
+++ omdbApiScraper.py
@@ -0,0 +1,33 @@
+#Import packages
+import pandas as pd # mov processing, CSV file I/O (e.g. pd.read_csv)
+import omdb #Package used to scrape movie mov from API
+import pymysql
+
+Connection = pymysql.connect(host='127.0.0.1', user="root", db='test', charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+
+mov = pd.read_sql("select * from movies", con=Connection)
+
+#Remove year from title and strip spaces from sides
+mov['title'] = mov['title'].str.replace(r"\(.*\)","")
+mov['title'] = mov['title'].str.strip()
+
+#Create new column and set index
+mov['Plot'] = mov.index
+
+for i in range(len(mov)):
+    print(i)
+    #Process API and create movframe
+    request = omdb.request(t=mov['title'][i], r='json', fullplot=True, ).content
+    dfPlot = pd.DataFrame(eval(request), columns=['Plot'], index=[0])
+    dfPlot.index.name = 'Index'
+
+    # Add values to dataframe and remove json brackets
+    mov['Plot'][i] = list(map(str, dfPlot['Plot'].values))
+    mov['Plot'][i] = ", ".join(mov['Plot'][i]) #Remove json brackets
+    
+
+#Insert df in 
+mov.to_sql(con=Connection, name='movies', if_exists='replace', flavor='mysql')
+
+#Close connection
+Connection.close()
